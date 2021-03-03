2ed77bcc6382569d21db9ce9626013bf463c7d03
IvanVargass <iivanvargas@hotmail.com>
2021-03-03 00:21:07 -0500

    Inicializando el repositorio

---
+++ Clase 1/comenzando.js
@@ -0,0 +1,16 @@
+
+//$(document).ready(()=>{
+    const btnEfectos = () =>{
+        /* $(".caja").hide(3000, ()=>{
+            alert("Caja oculta despues de 3s")
+        }); */
+        //$(".caja").show();
+        //$(".caja").toggle();
+        //$(".caja").fadeOut();
+        //$(".caja").fadeIn();
+        //$(".caja").fadeToggle();
+        $(".caja").slideToggle();
+        //$(".caja").slideDown();
+        //$(".caja").slideUp();
+    }
+//});
